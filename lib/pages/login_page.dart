51082c429db1ff62878cfa36e0a041c734c3d597
Alex <github@alexv525.com>
2021-04-05 04:37:21 +0800

    🎨 Improve login page's structure

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -17,50 +17,31 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  /// 是否允许登陆
-  final ValueNotifier<bool> _loginButtonEnabled = ValueNotifier<bool>(false);
-
-  /// 是否开启密码显示
-  final ValueNotifier<bool> _isObscure = ValueNotifier<bool>(true);
-
-  /// 是否正在登陆
-  final ValueNotifier<bool> _isRequesting = ValueNotifier<bool>(false);
-
-  /// 是否已勾选同意协议
-  final ValueNotifier<bool> _isAgreed = ValueNotifier<bool>(false);
+  late final _LoginNotifier _loginModel = _LoginNotifier();
 
   late final TextEditingController _usernameTEC = TextEditingController(
     text: SettingsUtil.getUserWorkId(),
   );
-  final TextEditingController _passwordTEC = TextEditingController();
-
-  String get _username => _usernameTEC.text;
-
-  String get _password => _passwordTEC.text;
 
   @override
   void dispose() {
-    _loginButtonEnabled.dispose();
-    _isObscure.dispose();
-    _isRequesting.dispose();
-    _isAgreed.dispose();
+    _loginModel.dispose();
     _usernameTEC.dispose();
-    _passwordTEC.dispose();
     super.dispose();
   }
 
   Future<void> login() async {
-    if (_isRequesting.value) {
+    if (_loginModel.isRequesting) {
       return;
     }
-    _isRequesting.value = true;
-    if (await UserAPI.login(_username, _password)) {
+    _loginModel.isRequesting = true;
+    if (await UserAPI.login(_loginModel.username, _loginModel.password)) {
       navigatorState.pushReplacement(
         MaterialPageRoute<void>(builder: (_) => const MainPage()),
       );
       return;
     }
-    _isRequesting.value = false;
+    _loginModel.isRequesting = false;
   }
 
   InputBorder _inputBorder(BuildContext context) {
@@ -76,42 +57,44 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget _userField(BuildContext context) {
     return _fieldWrapper(
-      child: ValueListenableBuilder<bool>(
-        valueListenable: _isRequesting,
-        builder: (_, bool isRequesting, __) => TextField(
-          controller: _usernameTEC,
-          enabled: !isRequesting,
-          decoration: InputDecoration(
-            border: _inputBorder(context),
-            enabledBorder: _inputBorder(context),
-            hintText: ' 工号/学号',
-            hintStyle: const TextStyle(fontWeight: FontWeight.normal),
-            suffixIcon: ValueListenableBuilder<TextEditingValue>(
-              valueListenable: _usernameTEC,
-              builder: (_, TextEditingValue value, __) {
-                if (isRequesting || value.text.isEmpty) {
-                  return const SizedBox.shrink();
-                }
-                return Tapper(
-                  onTap: _usernameTEC.clear,
-                  child: SvgPicture.asset(
-                    R.ASSETS_ICONS_CLEAR_INPUT_SVG,
-                    color: context.iconTheme.color,
-                  ),
-                );
-              },
-            ),
-            suffixIconConstraints: BoxConstraints.loose(Size.square(20.w)),
+      child: TextField(
+        controller: _usernameTEC,
+        enabled: !context.select<_LoginNotifier, bool>(
+          (_LoginNotifier p) => p.isRequesting,
+        ),
+        onChanged: (String value) => _loginModel.username = value,
+        decoration: InputDecoration(
+          border: _inputBorder(context),
+          enabledBorder: _inputBorder(context),
+          hintText: ' 工号/学号',
+          hintStyle: const TextStyle(fontWeight: FontWeight.normal),
+          suffixIcon: Consumer<_LoginNotifier>(
+            builder: (_, _LoginNotifier p, __) {
+              if (p.isRequesting || p.username.isEmpty) {
+                return const SizedBox.shrink();
+              }
+              return Tapper(
+                onTap: () {
+                  _usernameTEC.clear();
+                  _loginModel.username = '';
+                },
+                child: SvgPicture.asset(
+                  R.ASSETS_ICONS_CLEAR_INPUT_SVG,
+                  color: context.iconTheme.color,
+                ),
+              );
+            },
           ),
-          keyboardType: TextInputType.number,
-          inputFormatters: <TextInputFormatter>[
-            LengthLimitingTextInputFormatter(12),
-            FilteringTextInputFormatter.allow(RegExp(r'\d')),
-          ],
-          style: TextStyle(
-            fontSize: 16.sp,
-            fontWeight: FontWeight.bold,
-          ),
+          suffixIconConstraints: BoxConstraints.loose(Size.square(20.w)),
+        ),
+        keyboardType: TextInputType.number,
+        inputFormatters: <TextInputFormatter>[
+          LengthLimitingTextInputFormatter(12),
+          FilteringTextInputFormatter.allow(RegExp(r'\d')),
+        ],
+        style: TextStyle(
+          fontSize: 16.sp,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
@@ -119,42 +102,43 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget _passwordField(BuildContext context) {
     return _fieldWrapper(
-      child: ValueListenableBuilder2<bool, bool>(
-        firstNotifier: _isRequesting,
-        secondNotifier: _isObscure,
-        builder: (_, bool isRequesting, bool isObscure, __) => TextField(
-          controller: _passwordTEC,
-          enabled: !isRequesting,
-          obscureText: isObscure,
-          decoration: InputDecoration(
-            border: _inputBorder(context),
-            enabledBorder: _inputBorder(context),
-            hintText: ' 密码',
-            hintStyle: const TextStyle(fontWeight: FontWeight.normal),
-            suffixIcon: ValueListenableBuilder<TextEditingValue>(
-              valueListenable: _usernameTEC,
-              builder: (_, TextEditingValue value, __) {
-                if (isRequesting) {
-                  return const SizedBox.shrink();
-                }
-                return Tapper(
-                  onTap: () => _isObscure.value = !isObscure,
-                  child: SvgPicture.asset(
-                    isObscure
-                        ? R.ASSETS_ICONS_NOT_OBSCURE_SVG
-                        : R.ASSETS_ICONS_OBSCURE_SVG,
-                    color:
-                        isObscure ? context.iconTheme.color : defaultLightColor,
-                  ),
-                );
-              },
-            ),
-            suffixIconConstraints: BoxConstraints.loose(Size.square(20.w)),
-          ),
-          style: TextStyle(
-            fontSize: 16.sp,
-            fontWeight: FontWeight.bold,
-          ),
+      child: TextField(
+        enabled: !context.select<_LoginNotifier, bool>(
+          (_LoginNotifier p) => p.isRequesting,
+        ),
+        obscureText: context.select<_LoginNotifier, bool>(
+          (_LoginNotifier p) => p.isObscure,
+        ),
+        onChanged: (String value) => _loginModel.password = value,
+        decoration: InputDecoration(
+          border: _inputBorder(context),
+          enabledBorder: _inputBorder(context),
+          hintText: ' 密码',
+          hintStyle: const TextStyle(fontWeight: FontWeight.normal),
+          suffixIcon: () {
+            if (context.select<_LoginNotifier, bool>(
+              (_LoginNotifier p) => p.isRequesting,
+            )) {
+              return const SizedBox.shrink();
+            }
+            final bool isObscure = context.select<_LoginNotifier, bool>(
+              (_LoginNotifier p) => p.isObscure,
+            );
+            return Tapper(
+              onTap: () => _loginModel.isObscure = !isObscure,
+              child: SvgPicture.asset(
+                isObscure
+                    ? R.ASSETS_ICONS_NOT_OBSCURE_SVG
+                    : R.ASSETS_ICONS_OBSCURE_SVG,
+                color: isObscure ? context.iconTheme.color : defaultLightColor,
+              ),
+            );
+          }(),
+          suffixIconConstraints: BoxConstraints.loose(Size.square(20.w)),
+        ),
+        style: TextStyle(
+          fontSize: 16.sp,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
@@ -163,19 +147,19 @@ class _LoginPageState extends State<LoginPage> {
   Widget _agreementButton(BuildContext context) {
     return Row(
       children: <Widget>[
-        ValueListenableBuilder<bool>(
-          valueListenable: _isAgreed,
-          builder: (_, bool value, __) => Tapper(
+        Selector<_LoginNotifier, bool>(
+          selector: (_, _LoginNotifier p) => p.isAgreed,
+          builder: (BuildContext context, bool isAgreed, __) => Tapper(
             onTap: () {
-              _isAgreed.value = !value;
+              _loginModel.isAgreed = !isAgreed;
             },
             child: SvgPicture.asset(
-              value
+              isAgreed
                   ? R.ASSETS_ICONS_AGREEMENT_AGREED_SVG
                   : R.ASSETS_ICONS_AGREEMENT_SVG,
               width: 20.w,
               height: 20.w,
-              color: !value ? context.theme.dividerColor : null,
+              color: !isAgreed ? context.theme.dividerColor : null,
             ),
           ),
         ),
@@ -211,99 +195,161 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _loginButton(BuildContext context) {
-    return ValueListenableBuilder2<bool, bool>(
-      firstNotifier: _loginButtonEnabled,
-      secondNotifier: _isRequesting,
-      builder: (_, bool enabled, bool isRequesting, __) => ThemeTextButton(
-        onPressed: enabled ? login : null,
+    return Selector<_LoginNotifier, bool>(
+      selector: (_, _LoginNotifier p) => p.isEnabled,
+      builder: (BuildContext context, bool isEnabled, _) => ThemeTextButton(
+        onPressed: isEnabled ? login : null,
         text: '登录',
-        child: isRequesting
-            ? SizedBox(
-                width: 30.w,
-                child: PlatformProgressIndicator(
-                  alignment: Alignment.center,
-                  size: Size.square(22.w),
-                  color: Colors.white,
-                  strokeWidth: 3,
-                ),
-              )
-            : null,
+        child: () {
+          if (context.select<_LoginNotifier, bool>(
+            (_LoginNotifier p) => p.isRequesting,
+          )) {
+            return SizedBox(
+              width: 30.w,
+              child: PlatformProgressIndicator(
+                alignment: Alignment.center,
+                size: Size.square(22.w),
+                color: Colors.white,
+                strokeWidth: 3,
+              ),
+            );
+          }
+          return null;
+        }(),
       ),
-    );
-  }
-
-  /// This is pretty much a magic by checking fields using VLBs, and calls the
-  /// value update at post frame, without add listeners to controllers.
-  Widget get _fieldsCheck {
-    return ValueListenableBuilder3<bool, TextEditingValue, TextEditingValue>(
-      firstNotifier: _isAgreed,
-      secondNotifier: _usernameTEC,
-      thirdNotifier: _passwordTEC,
-      builder: (
-        _,
-        bool isAgreed,
-        TextEditingValue uValue,
-        TextEditingValue pValue,
-        Widget? child,
-      ) {
-        WidgetsBinding.instance!.addPostFrameCallback((_) {
-          _loginButtonEnabled.value =
-              isAgreed && uValue.text.isNotEmpty && pValue.text.isNotEmpty;
-        });
-        return child!;
-      },
-      child: const SizedBox.shrink(),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => false,
-      child: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: currentIsDark
-            ? SystemUiOverlayStyle.light
-            : SystemUiOverlayStyle.dark,
-        child: Scaffold(
-          body: SafeArea(
-            child: Padding(
-              padding: EdgeInsets.all(40.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    '欢迎体验全新 JMU Tools',
-                    style: TextStyle(
-                      fontSize: 22.sp,
-                      fontWeight: FontWeight.bold,
+    return ChangeNotifierProvider<_LoginNotifier>.value(
+      value: _loginModel,
+      builder: (BuildContext context, _) => WillPopScope(
+        onWillPop: () async => false,
+        child: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.dark,
+          child: Scaffold(
+            body: SafeArea(
+              child: Padding(
+                padding: EdgeInsets.all(40.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      '欢迎体验全新 JMU Tools',
+                      style: TextStyle(
+                        fontSize: 22.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Text(
-                    '请先登录您的账号',
-                    style: context.textTheme.caption!.copyWith(
-                      fontSize: 14.sp,
+                    Text(
+                      '请先登录您的账号',
+                      style: context.textTheme.caption!.copyWith(
+                        fontSize: 14.sp,
+                      ),
                     ),
-                  ),
-                  VGap(30.w),
-                  _userField(context),
-                  _passwordField(context),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20.w),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(child: _agreementButton(context)),
-                        _loginButton(context),
-                      ],
+                    VGap(30.w),
+                    _userField(context),
+                    _passwordField(context),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20.w),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(child: _agreementButton(context)),
+                          _loginButton(context),
+                        ],
+                      ),
                     ),
-                  ),
-                  _fieldsCheck,
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class _LoginNotifier extends ChangeNotifier {
+  _LoginNotifier({
+    String? username,
+    String? password,
+    bool isAgreed = false,
+    bool isRequesting = false,
+    bool isObscure = true,
+  }) {
+    _username = username ?? SettingsUtil.getUserWorkId() ?? '';
+    _password = password ?? '';
+    _isAgreed = isAgreed;
+    _isRequesting = isRequesting;
+    _isObscure = isObscure;
+  }
+
+  String _username = '';
+
+  String get username => _username;
+
+  set username(String value) {
+    if (value == _username) {
+      return;
+    }
+    _username = value;
+    notifyListeners();
+  }
+
+  String _password = '';
+
+  String get password => _password;
+
+  set password(String value) {
+    if (value == _password) {
+      return;
+    }
+    _password = value;
+    notifyListeners();
+  }
+
+  bool _isAgreed = false;
+
+  bool get isAgreed => _isAgreed;
+
+  set isAgreed(bool value) {
+    if (value == _isAgreed) {
+      return;
+    }
+    _isAgreed = value;
+    notifyListeners();
+  }
+
+  bool get isEnabled =>
+      _username.isNotEmpty &&
+      _password.isNotEmpty &&
+      _isAgreed &&
+      !_isRequesting;
+
+  bool _isRequesting = false;
+
+  bool get isRequesting => _isRequesting;
+
+  set isRequesting(bool value) {
+    if (value == _isRequesting) {
+      return;
+    }
+    _isRequesting = value;
+    notifyListeners();
+  }
+
+  bool _isObscure = true;
+
+  bool get isObscure => _isObscure;
+
+  set isObscure(bool value) {
+    if (value == _isObscure) {
+      return;
+    }
+    _isObscure = value;
+    notifyListeners();
   }
 }
